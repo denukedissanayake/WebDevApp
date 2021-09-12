963ba017fb5b6fd93a2b4165cc48a9c607dacb84
Denuke <ddnadhananja@gmail.com>
2021-09-13 01:26:51 +0530

    Added update delete ooks

--- routes/books.js
+++ routes/books.js
@@ -53,16 +53,82 @@ router.post('/', async (req, res) => {
 
     try {
         const newBook = await book.save()
-        //  res.redirect(`books/${newBook.id}`)
-        res.redirect(`books`)
+        res.redirect(`books/${newBook.id}`)
+        // res.redirect(`books`)
 
     } catch (error) { 
         renderNewPage(res, book, true)
     }
 })
 
+router.get('/:id', async (req, res) => {
+    try {
+        const book = await Book.findById(req.params.id).populate('author').exec()
+        res.render('books/show', {book: book})
+    } catch {
+        res.redirect('/')
+    }
+})
 
-async function renderNewPage(res, book, hasError = false) {
+router.get('/:id/edit', async (req, res) => {
+
+    try {
+        const book = await Book.findById(req.params.id)
+        renderEditPage(res, book)
+    } catch {
+        res.redirect('/')
+    }
+})
+
+router.put('/:id', async (req, res) => {
+
+    let book
+
+    try {
+        book = await Book.findById(req.params.id)
+        book.title = req.body.title
+        book.author = req.body.author
+        book.publishedDate = new Date(req.body.publishedDate)
+        book.pageCount = req.body.pageCount
+        book.description = req.body.description
+
+        if (req.body.cover && req.body.cover !=='') {
+            saveCover(book, req.body.cover)
+        }
+
+        await book.save()
+        res.redirect(`/books/${book.id}`)
+
+    } catch {
+
+        if (book) {
+            renderEditPage(res, book, true)
+        } else {
+            res.redirect('/')
+        }
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    let book
+    try {
+        book = await Book.findById(req.params.id)
+        await book.remove()
+        res.redirect('/books')
+    } catch {
+        if (book) {
+            res.render('/books/show', {
+                book: book,
+                errorMessage: 'Could not Delete the Book'
+            })
+        } else {
+            res.redirect('/')
+        }
+    }
+})
+
+
+async function renderFormPage(res, book, form, hasError = false) {
     try {
         const authors = await Author.find({})
 
@@ -71,19 +137,28 @@ async function renderNewPage(res, book, hasError = false) {
             book: book
         }
         if (hasError) {
-            params.errorMessage = 'Error Creating a Book!'
+            if (form == 'edit') {
+                params.errorMessage = 'Error Updating the Book!'
+            } else {
+                params.errorMessage = 'Error Creating a Book!'
+            }
         }
-        res.render('books/new', params)
+        res.render(`books/${form}`, params)
     } catch {
         res.redirect('/books')
     }
 }
 
-// function removeBookCover(fileName) {
-//     fs.unlink(path.join(uploadPath, fileName), err => {
-//         if(err) console.log(err)
-//     })
-// }
+
+
+async function renderNewPage(res, book, hasError = false) {
+    renderFormPage(res, book, 'new', hasError)
+}
+
+async function renderEditPage(res, book, hasError = false) {
+    renderFormPage(res, book, 'edit', hasError)
+}
+
 
 function saveCover(book, coverEncoded) {
     if (coverEncoded) {
